3593a5df96dba543cb9255bb1fba5349f3254153
JackMAlucard <jack.rojas@epn.edu.ec>
2024-02-15 08:23:15 -0500

    Initial refactor of m0_utilities. Testing, and definitive consistent spacing and descriptions still pending.

--- src/m0_utilities.f90
+++ src/m0_utilities.f90
@@ -1,130 +1,120 @@
-module m0
-implicit none
+module m0_utilities
+	implicit none
 
-!NUMERICAL STORAGE SIZE PARAMETERS FOR REAL AND INTEGER VALUES******************
-!Single precision real numbers, 6 digits, range 10**(-37) to 10**(37)-1; 32 bits
-integer, parameter :: sp = selected_real_kind(6, 37)
-!Double precision real numbers, 15 digits, range 10**(-307) to 10**(307)-1; 64 bits
-integer, parameter :: dp = selected_real_kind(15, 307)
-!Quadruple precision real numbers, 33 digits, range 10**(-4931) to 10**(4931)-1; 128 bits
-integer, parameter :: qp = selected_real_kind(33, 4931)
+	! NUMERICAL STORAGE SIZE PARAMETERS FOR REAL AND INTEGER VALUES***************
+	! Double precision real numbers,
+	! 15 digits, range 10**(-307) to 10**(307)-1; 64 bits
+	integer, parameter :: dp = selected_real_kind(15, 307)
+	! Long length for integers, range -2**63 to 2**63-1; 64 bits
+	integer, parameter :: i8 = selected_int_kind(18)
 
-!Char length for integers, range -2**7 to 2**7-1; 8 bits
-integer, parameter :: i1 = selected_int_kind(2)
-!Short length for integers, range -2**15 to 2**15-1; 16 bits
-integer, parameter :: i2 = selected_int_kind(4)
-!Length of default integers, range -2**31 to 2**31-1; 32 bits
-integer, parameter :: i4 = selected_int_kind(9)
-!Long length for integers, range -2**63 to 2**63-1; 64 bits
-integer, parameter :: i8 = selected_int_kind(18)
+	! NUMERICAL CONSTANTS*********************************************************
+	real(dp), parameter :: PI = dacos(-1._dp)
 
-real(dp), parameter :: PI = dacos(-1._dp)
+	contains
+	! UNIT CONVERSION SUBROUTINES*************************************************
+	! Energy conversion from keV to hartree Eh (atomic)
+	!	real(dp) function EinAU(EinkeV)
+	subroutine kev_to_atomic_energy_conversion(energy)
+		implicit none
+		real(dp), intent(inout) :: energy
+		energy = energy*1.d3/27.21139
+	end subroutine kev_to_atomic_energy_conversion
+	
+	! Distance conversion from meters m (SI) to Bohr radii a0 (atomic)
+	! real(dp) function dinAUfromSI(dinSI)
+	subroutine si_to_atomic_distance_conversion(distance)
+		implicit none
+		real(dp), intent(inout) :: distance
+		distance = distance/0.5291772d-10
+	end subroutine si_to_atomic_distance_conversion
 
-contains
-!UNIT CONVERSION SUBROUTINES****************************************************
-!Energy conversion from keV to Eh (au)
-real(dp) function EinAU(EinkeV)
-implicit none
-real(dp), intent(in) :: EinkeV
-	EinAU = EinkeV*1.d3/27.21139
-end function EinAU
+	! Distance conversion from angstroms to Bohr radii a0 (atomic)
+!	real(dp) function dinAU(dinangstroms)
+	subroutine angstrom_to_atomic_distance_conversion(distance)
+		implicit none
+		real(dp), intent(inout) :: distance
+		distance = distance/0.5291772
+	end subroutine angstrom_to_atomic_distance_conversion
 
-!Distance conversion from angstroms to Bohr radii a0 (au)
-real(dp) function dinAU(dinangstroms)
-implicit none
-real(dp), intent(in) :: dinangstroms
-	dinAU = dinangstroms/0.5291772
-end function dinAU
+	! Distance conversion from Bohr radii a0 (atomic) to meters (SI)
+!	real(dp) function dinSI(dinAU)
+	subroutine atomic_to_si_distance_conversion(distance)
+		implicit none
+		real(dp), intent(inout) :: distance
+		distance = distance*0.5291772d-10
+	end subroutine atomic_to_si_distance_conversion
 
-!Distance conversion from Bohr radii a0 (au) to meters (SI)
-real(dp) function dinSI(dinAU)
-implicit none
-real(dp), intent(in) :: dinAU
-	dinSI = dinAU*0.5291772d-10
-end function dinSI
+	! RANDOM NUMBER GENERATION SUBROUTINES****************************************
+	! Subroutine which generates a random number following the standard uniform
+	! distribution over the range (0,1] to avoid possible error if 0 is generated.
+	! Adapted from: https://masuday.github.io/fortran_tutorial/random.html
+	subroutine random_std_uniform(u)
+		implicit none
+		real(dp), intent(out) :: u
+		real(dp) :: r
+		call random_number(r)
+		u = 1 - r
+	end subroutine random_std_uniform
 
-!Distance conversion from meters (SI) to Bohr radii a0 (au)
-real(dp) function dinAUfromSI(dinSI)
-implicit none
-real(dp), intent(in) :: dinSI
-	dinAUfromSI = dinSI/0.5291772d-10
-end function dinAUfromSI
+	! Subroutine which generates two random numbers following the standard 
+	! normal distribution (mean = 0 and standard deviation = 1).
+	! Adapted from: https://masuday.github.io/fortran_tutorial/random.html
+	subroutine random_std_normal(z1, z2)
+		real(dp), intent(out) :: z1, z2
+		real(dp) :: u1, u2
+		call random_std_uniform(u1)
+		call random_std_uniform(u2)
+		z1 = dsqrt(-2*dlog(u1))*dcos(2*PI*u2)
+		z2 = dsqrt(-2*dlog(u1))*dsin(2*PI*u2)
+	end subroutine random_std_normal
 
-!PRN GENERATION SUBROUTINES*****************************************************
-!Subroutine which generates a pseudorandom number from the standard uniform
-!distribution over the range (0,1] to avoid possible error if 0 is generated.
-!Adapted from https://masuday.github.io/fortran_tutorial/random.html
-subroutine random_stduniform(u)
-real(dp), intent(out) :: u
-real(dp) :: r
-	call RANDOM_NUMBER(r)
-	u = 1 - r
-end subroutine random_stduniform
+	! Subroutine which generates two random number following the general
+	! normal distribution with mean 'mu' and standard deviation 'sigma'.
+	subroutine random_normal(mu, sigma, x1, x2)
+		real(dp), intent(in) :: mu, sigma
+		real(dp), intent(out) :: x1, x2
+		real(dp) :: z1, z2
+		call random_stdnormal(z1, z2)
+		x1 = mu + sigma*z1
+		x2 = mu + sigma*z2
+	end subroutine random_normal
 
-!Subroutine which generates a pseudorandom number from the uniform
-!distribution over the range [a,b), assuming a<b.
-!Adapted from https://masuday.github.io/fortran_tutorial/random.html
-subroutine random_uniform(x,a,b)
-real(dp), intent(out) :: x
-real(dp), intent(in) :: a, b
-real(dp) :: r
-	call RANDOM_NUMBER(r)
-	x = (b-a)*r + a
-end subroutine random_uniform
+	! Subroutine which generates a random number following the exponential
+	! probability distribution with parameter 'lambda' > 0.
+	! Adapted from:
+	! https://www.eg.bucknell.edu/~xmeng/Course/CS6337/Note/master/node50.html
+	subroutine random_exponential(lambda, x)
+		real(dp), intent(in) :: lambda
+		real(dp), intent(out) :: x
+		real(dp) :: r
+		call random_std_uniform(u)
+		x = -dlog(u)/lambda
+	end subroutine random_exponential
 
-!Subroutine which generates two pseudorandom number from the normal
-!distribution (mean=0 and standard deviation=1).
-!Modified from https://masuday.github.io/fortran_tutorial/random.html
-subroutine random_stdnormal(x1,x2)
-real(dp), intent(out) :: x1, x2
-real(dp) :: u1, u2
-	call random_stduniform(u1)
-	call random_stduniform(u2)
-	x1 = dsqrt(-2*dlog(u1))*dcos(2*PI*u2)
-	x2 = dsqrt(-2*dlog(u1))*dsin(2*PI*u2)
-end subroutine random_stdnormal
+	!VECTOR ROTATION ALONG THE X-AXIS*********************************************
+!	subroutine matrix_vector_product(M, V)
+	subroutine rotation_about_x_axis(theta, v)
+		real(dp), intent(in) :: angle
+		real(dp), intent(inout) :: v(3)
+		real(dp) :: Rx(3,3), v_aux(3)
+		integer :: i, j
+		
+		!Rotation matrix
+		R(1,:) = (/1._dp, 0._dp, 0._dp/)
+		R(2,:) = (/0._dp, dcos(theta), -dsin(theta)/)
+		R(3,:) = (/0._dp, dsin(theta), dcos(theta)/)
+		
+		v_aux = 0._dp
 
-!Subroutine which generates two pseudorandom number from the general
-!normal distribution (mean=mu and standard deviation=sigma).
-subroutine random_normal(x1,x2,mu,sigma)
-real(dp), intent(out) :: x1, x2
-real(dp), intent(in) :: mu, sigma
-real(dp) :: u1, u2
-	call random_stdnormal(u1,u2)
-	x1 = u1*sigma + mu
-	x2 = u2*sigma + mu
-end subroutine random_normal
-
-! Subroutine which generates a pseudorandom number following the exponential
-! probability distribution with parameter lambda > 0 and mean 1/lambda.
-! Adapted from:
-! https://www.eg.bucknell.edu/~xmeng/Course/CS6337/Note/master/node50.html
-subroutine random_exponential(x,lambda)
-real(dp), intent(out) :: x
-real(dp), intent(in) :: lambda
-real(dp) :: r
-	call random_stduniform(r)
-	x = -dlog(r)/lambda
-end subroutine random_exponential
-
-!MATRIX PRODUCT SUBRUTINE*******************************************************
-subroutine matrix_vector_product(M, V)
-real(dp), intent(in) :: M(:,:)
-real(dp), intent(inout) :: V(:)
-real(dp), allocatable :: V_aux(:)
-integer :: N, i, j
-	N = size(V)
-	allocate(V_aux(N))
-	V_aux = 0._dp
-
-	do i=1, N
-		do j=1, N
-			V_aux(i) = V_aux(i) + M(i,j)*V(j)
+		do i = 1, 3
+			do j = 1, 3
+				v_aux(i) = v_aux(i) + Rx(i,j)*v(j)
+			end do
 		end do
-	end do
 
-	V = V_aux
-	deallocate(V_aux)
-end subroutine matrix_vector_product
+		v = v_aux
 
-end module m0
+	end subroutine rotation_about_x_axis
+
+end module m0_utilities
