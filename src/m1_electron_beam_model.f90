6c146d5be82777495e49baba651019094c745991
JackMAlucard <jack.rojas@epn.edu.ec>
2024-02-18 16:08:26 -0500

    Updated m1_electron_beam_model
    Set subroutine definition withint the 80 characters limit.
    
    Added alternative longer name but kept it commented.
    
    Reorganized unit conversions so that they are all on the same place.
    
    Renamed and reorganized mu and sigma values for the positions and
    energy distributions.
    
    Renamed the electron_energy_1 and 2 variables to just energy_1 and 2.
    
    Defined translation_vector with a literal name now, included the use
    of a vector_translation subroutine instead of doing the translation
    directly in the code.
    
    Fixed tranformation operations order.

--- src/m1_electron_beam_model.f90
+++ src/m1_electron_beam_model.f90
@@ -20,90 +20,94 @@ module m1_electron_beam_model
 ! The subroutine converts the magnitudes to the atomic system of units.
 ! The vectors generated are in the beam reference system, the 'primed' system.
 ! subroutine electron_beam(e_pos, e_vel, e_acc, N_in, E_in, R_in, prob_dist_in)
-  subroutine setup_electron_beam(num_electrons, spot_size, beam_energy &
-                                 beam_spread, grazing_angle, distance_to_target &
-                                 electron_positions, electron_velocities &
+  subroutine setup_electron_beam(num_electrons, spot_size, &
+                                 beam_energy, beam_spread, &
+                                 distance_to_target, grazing_angle, &
+                                 electron_positions, electron_velocities, &
                                  electron_accelerations)
-! subroutine setup_electron_beam_model(...)                                  
-!subroutine electron_beam(N, E, R, eb_pos, eb_vel, eb_acc)
-!subroutine electron_beam(N, E, R, eb_r, eb_v, eb_a)
-!subroutine electron_beam(N, E, R, r_eb, v_eb, a_eb)
-!real(dp), allocatable, intent(out) :: eb_pos(:,:), eb_vel(:,:), eb_acc(:,:)
-integer(i8), intent(in) :: num_electrons
-real(dp), intent(in) :: spot_size, beam_energy, beam_spread
-real(dp), intent(in) :: grazing_angle, distance_to_target
-real(dp), allocatable, intent(out) :: electron_positions(:,:)
-real(dp), allocatable, intent(out) :: electron_velocities(:,:)
-real(dp), allocatable, intent(out) :: electron_accelerations(:,:)
-real(dp) :: beam_mu, beam_sigma
-real(dp) :: x, y, z, vx, vy, vz
-real(dp) :: Ei1, Ei2
-integer(i8) :: i
+!  subroutine setup_electron_beam_model(num_electrons, spot_size, &
+!                                       beam_energy, beam_spread, &
+!                                       distance_to_target, &
+!                                       grazing_angle, &
+!                                       electron_positions, &
+!                                       electron_velocities, &
+!                                       electron_accelerations)
+	!subroutine electron_beam(N, E, R, eb_pos, eb_vel, eb_acc)
+	!subroutine electron_beam(N, E, R, eb_r, eb_v, eb_a)
+	!subroutine electron_beam(N, E, R, r_eb, v_eb, a_eb)
+		!real(dp), allocatable, intent(out) :: eb_pos(:,:), eb_vel(:,:), eb_acc(:,:)
+		integer(i8), intent(in) :: num_electrons
+		real(dp), intent(in) :: spot_size, beam_energy, beam_spread
+		real(dp), intent(in) :: distance_to_target, grazing_angle
+		real(dp), allocatable, intent(out) :: electron_positions(:,:)
+		real(dp), allocatable, intent(out) :: electron_velocities(:,:)
+		real(dp), allocatable, intent(out) :: electron_accelerations(:,:)
+		real(dp) :: positions_mu, positions_sigma, energy_mu, energy_sigma
+		real(dp) :: x, y, z, vx, vy, vz
+		real(dp) :: energy_1, energy_2
+		integer(i8) :: i
 
-	!Unit conversion
-	call kev_to_atomic_energy_conversion(beam_energy)
-	call angstrom_to_atomic_distance_conversion(spot_size)
-
-	allocate(electron_positions(num_electrons,3))
-	allocate(electron_velocities(num_electrons,3))
-	allocate(electron_accelerations(num_electrons,3))
-	
-	!They follow the normal distribution with
-	!Full Width at Tenth of Maximum (FWTM):
-	!FWTM = 2*sqrt(2*ln(10))*sigma ~ 4.29193*sigma
-	beam_mu = 0
-	beam_sigma = spot_size/(2*dsqrt(2*dlog(10._dp)))
-
-	do i = 1, N
-		!Generate positions
-		!x and y coordinates
-		call random_normal(beam_mu, beam_sigma, x, y)
-		!z coordinate
-		z = 0
-		electron_positions(i,:) = (/x, y, z/)
-
-		!Generate velocities
-		!For 10kev, the speed is ~ 0.19783585 c = 59350755.095 m/s
-		!Or 58455098 m/s = 0.194850327 c (with Lorentz tranform?)
-		vx = 0
-		vy = 0
-
-		!Since two numbers are generated in each call of random_normal
-		!Using this if-else structure I avoid generating more numbers than necessary
-		if (mod(i+1,2) .eq. 0) then
-			! Electron energy values follow a normal distribution 
-			! with a mean of E, and a mean deviation of 0.1*E
-			call random_normal(beam_energy,(energy_spread/100)*beam_energy
-			                   electron_energy_1, electron_energy_2)
-			vz = dsqrt(2*electron_energy_1)
-		else
-			vz = dsqrt(2*electron_energy_2)
-		end if
-		electron_velocities(i,:) = (/vx, vy, vz/)
-
-		!Initialize accelerations
-		electron_accelerations(i,:) = 0
-		
-		!Transforming electron beam
-		
-		!Translation vector
-		T = (/0._dp, 0._dp, -distance_to_target/)
-		
 		!Unit conversion
-		grazing_angle = grazing_angle*PI/180
+		call angstrom_to_atomic_distance_conversion(spot_size)
+		call kev_to_atomic_energy_conversion(beam_energy
 		call angstrom_to_atomic_distance_conversion(distance_to_target)
-		
-		do i = 1, num_electrons
-			!Rotation around x-axis
-			call rotation_about_x_axis(grazing_angle, electron_positions(i,:))
-			call rotation_about_x_axis(grazing_angle, electron_velocities(i,:))
+		grazing_angle = grazing_angle*PI/180
 
-			!Initial positions translation
-			electron_positions(i,:) = electron_positions(i,:) + T
+		allocate(electron_positions(num_electrons,3))
+		allocate(electron_velocities(num_electrons,3))
+		allocate(electron_accelerations(num_electrons,3))
+		
+		!Electron positions follow a normal distribution with
+		!Full Width at Tenth of Maximum (FWTM):
+		!FWTM = 2*sqrt(2*ln(10))*sigma ~ 4.29193*sigma
+		positions_mu = 0
+		positions_sigma = spot_size/(2*dsqrt(2*dlog(10._dp)))
+		
+		!Electron energies follow a normal distribution with
+		energy_mu = beam_energy
+		energy_sigma = (energy_spread/100)*beam_energy
+
+		do i = 1, N
+			!Generating positions
+			!x and y coordinates
+			call random_normal(beam_mu, beam_sigma, x, y)
+			!z coordinate
+			z = 0
+			electron_positions(i,:) = (/x, y, z/)
+
+			!Generating velocities
+			vx = 0
+			vy = 0
+
+			!Since two numbers are generated in each call of random_normal
+			!Using this if-else structure I avoid generating unnecessary numbers
+			if (mod(i+1,2) .eq. 0) then
+				call random_normal(energy_mu, energy_sigma, energy_1, energy_2)
+				vz = dsqrt(2*energy_1)
+			else
+				vz = dsqrt(2*energy_2)
+			end if
+			electron_velocities(i,:) = (/vx, vy, vz/)
+
+			!Initializing accelerations
+			electron_accelerations(i,:) = 0
+			
+			!Transforming electron beam
+			translation_vector = (/0._dp, 0._dp, -distance_to_target/)
+			
+			do i = 1, num_electrons
+				!Initial positions translation
+				call vector_translation(traslation_vector, electron_positions(i,:))
+!				electron_positions(i,:) = electron_positions(i,:) + T
+				
+				!Rotation around x-axis
+				call rotation_about_x_axis(grazing_angle, electron_positions(i,:))
+				call rotation_about_x_axis(grazing_angle, electron_velocities(i,:))
+				
+			end do
+			
 		end do
-		
-	end do
 
-end subroutine setup_electron_beam
+	end subroutine setup_electron_beam
 
 end module m1_electron_beam_model
