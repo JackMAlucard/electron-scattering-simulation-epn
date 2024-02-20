a99fb9dfe8048c590b6de5395808510addd80cea
JackMAlucard <jack.rojas@epn.edu.ec>
2024-02-20 08:34:46 -0500

    Fixed error: Transformation was inside a loop. Added indenting.

--- src/m1_electron_beam_model.f90
+++ src/m1_electron_beam_model.f90
@@ -2,24 +2,23 @@ module m1_electron_beam_model
 	implicit none
 	use m0!, only: sp, dp, i8,...
 	contains
-!INDENTING EVERYTHING INSIDE HERE PENDING...
 
-! Subroutine which generates an array with N electron random starting positions
-! following a normal distribution whose Full Width at Tenth Maximum (FWTM) 
-! represents the spot size (beam diameter) of the beam.
-! The electrons are set up to have initial velocities pointing in 
-! the +z direction with values following a normal distribution of mean equal 
-! to the beam primary energy. The initial accelerations are null.
-! The electron beam is set up to have a grazing incidence angle psi and
-! located a distance d from the target dielectric material.
-! The subroutine parameters are the number of electrons num_electrons, 
-! the beam primary energy beam_energy (in keV), the beam spread (as a % of
-! the beam primary energy, the spot size (beam diameter) spot_size (in Å), 
-! the grazing incidence angle grazing_angle (in degrees), and distance 
-! to the target material distance_to_target (in Å).
-! The subroutine converts the magnitudes to the atomic system of units.
-! The vectors generated are in the beam reference system, the 'primed' system.
-! subroutine electron_beam(e_pos, e_vel, e_acc, N_in, E_in, R_in, prob_dist_in)
+	! Subroutine which generates an array with N electron random starting positions
+	! following a normal distribution whose Full Width at Tenth Maximum (FWTM) 
+	! represents the spot size (beam diameter) of the beam.
+	! The electrons are set up to have initial velocities pointing in 
+	! the +z direction with values following a normal distribution of mean equal 
+	! to the beam primary energy. The initial accelerations are null.
+	! The electron beam is set up to have a grazing incidence angle psi and
+	! located a distance d from the target dielectric material.
+	! The subroutine parameters are the number of electrons num_electrons, 
+	! the beam primary energy beam_energy (in keV), the beam spread (as a % of
+	! the beam primary energy, the spot size (beam diameter) spot_size (in Å), 
+	! the grazing incidence angle grazing_angle (in degrees), and distance 
+	! to the target material distance_to_target (in Å).
+	! The subroutine converts the magnitudes to the atomic system of units.
+	! The vectors generated are in the beam reference system, the 'primed' system.
+	! subroutine electron_beam(e_pos, e_vel, e_acc, N_in, E_in, R_in, prob_dist_in)
 	!subroutine electron_beam(N, E, R, eb_pos, eb_vel, eb_acc)
 	!subroutine electron_beam(N, E, R, eb_r, eb_v, eb_a)
 	!subroutine electron_beam(N, E, R, r_eb, v_eb, a_eb)
@@ -47,7 +46,7 @@ module m1_electron_beam_model
 		real(dp) :: traslation_vector(3)
 		integer(i8) :: i
 
-		!Unit conversion
+		! Unit conversion
 		call angstrom_to_atomic_distance_conversion(spot_size)
 		call kev_to_atomic_energy_conversion(beam_energy)
 		call angstrom_to_atomic_distance_conversion(distance_to_target)
@@ -57,30 +56,30 @@ module m1_electron_beam_model
 		allocate(electron_velocities(num_electrons,3))
 		allocate(electron_accelerations(num_electrons,3))
 		
-		!Electron positions follow a normal distribution with
-		!Full Width at Tenth of Maximum (FWTM):
-		!FWTM = 2*sqrt(2*ln(10))*sigma ~ 4.29193*sigma
+		! Electron positions follow a normal distribution with
+		! Full Width at Tenth of Maximum (FWTM):
+		! FWTM = 2*sqrt(2*ln(10))*sigma ~ 4.29193*sigma
 		positions_mu = 0
 		positions_sigma = spot_size/(2*dsqrt(2*dlog(10._dp)))
 		
-		!Electron energies follow a normal distribution with
+		! Electron energies follow a normal distribution with
 		energy_mu = beam_energy
 		energy_sigma = (energy_spread/100)*beam_energy
 
-		do i = 1, N
-			!Generating positions
-			!x and y coordinates
+		do i = 1, num_electrons
+			! Generating positions
+			! x and y coordinates
 			call random_normal(beam_mu, beam_sigma, x, y)
-			!z coordinate
+			! z coordinate
 			z = 0
 			electron_positions(i,:) = (/x, y, z/)
 
-			!Generating velocities
+			! Generating velocities
 			vx = 0
 			vy = 0
 
-			!Since two numbers are generated in each call of random_normal
-			!Using this if-else structure I avoid generating unnecessary numbers
+			! Since two numbers are generated in each call of random_normal
+			! Using this if-else structure I avoid generating unnecessary numbers
 			if (mod(i+1,2) .eq. 0) then
 				call random_normal(energy_mu, energy_sigma, energy_1, energy_2)
 				vz = dsqrt(2*energy_1)
@@ -88,23 +87,23 @@ module m1_electron_beam_model
 				vz = dsqrt(2*energy_2)
 			end if
 			electron_velocities(i,:) = (/vx, vy, vz/)
-
-			!Initializing accelerations
+			
+			! Initializing accelerations
 			electron_accelerations(i,:) = 0
 			
-			!Transforming electron beam
-			translation_vector = (/0._dp, 0._dp, -distance_to_target/)
-			
-			do i = 1, num_electrons
-				!Initial positions translation
-				call vector_translation(traslation_vector, electron_positions(i,:))
+		end do
+				
+		! Transforming electron beam vectors
+		translation_vector = (/0._dp, 0._dp, -distance_to_target/)
+		
+		do i = 1, num_electrons
+			! Initial positions translation
+			call vector_translation(traslation_vector, electron_positions(i,:))
 !				electron_positions(i,:) = electron_positions(i,:) + T
-				
-				!Rotation around x-axis
-				call rotation_about_x_axis(grazing_angle, electron_positions(i,:))
-				call rotation_about_x_axis(grazing_angle, electron_velocities(i,:))
-				
-			end do
+			
+			! Rotation around x-axis
+			call rotation_about_x_axis(grazing_angle, electron_positions(i,:))
+			call rotation_about_x_axis(grazing_angle, electron_velocities(i,:))
 			
 		end do
 
