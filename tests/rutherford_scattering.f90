03bd3ef0ecdfb46878a0c3beac77ef63fa069a8a
JackMAlucard <jack_marco_rojas@hotmail.com>
2025-05-12 08:56:08 -0500

    Finished grouping main program variables for clarity refactor

--- tests/rutherford_scattering.f90
+++ tests/rutherford_scattering.f90
@@ -6,6 +6,7 @@ program rutherfhord_scattering_test_simulations
 	
 	use, intrinsic:: iso_fortran_env, only: stdin=>input_unit
 	implicit none
+	! NUMERICAL CONSTANTS
 	! Kind type parameters for increased real precision and integer length
 	! Single precision reals, 6 digits, range 10**(-37) to 10**(37)-1; 32 bits
 	integer, parameter :: sp = selected_real_kind(6, 37)
@@ -13,38 +14,59 @@ program rutherfhord_scattering_test_simulations
 	integer, parameter :: dp = selected_real_kind(15, 307)
 	! Long length for integers, range -2**63 to 2**63-1; 64 bits
 	integer, parameter :: i8 = selected_int_kind(18)
-	! General variables
+	! General constants
 	real(dp), parameter :: PI = dacos(-1.d0)
 	real(dp), parameter :: t0 = 0._dp
-	integer(i8) :: i, j	!Program structure parameters
-	integer(i8) :: num_plot_ploints	!Number of points to be plotted/simulated
-	real(dp) :: K0, K0f, x0, x0f	!Fixed simulation parameters
-	real(dp) :: b, dt							!Variable simulation parameters
-	real(dp) :: rt(3)						!Target electron position
-	real(dp) :: r0(3), v0(3)		!Simulation position and velocity
-	integer(i8) :: max_iterations	!Number of iterations
-	real(dp) :: a, c, e			!Theoretical trajectory geometric parameters
-	real(dp) :: xf, yf		!Last theoretical trajectory point generated
-	real(dp) :: scattering_angle_theoretical
-	real(dp) :: scattering_angle_plot, scattering_angle_simulation
-	real(dp) :: ti, ri(3)	!Time and space simulation variables
-	real(dp) :: vi(3)			!Velocity simulation variables
-	real(dp) :: ai(3) 		!Acceleration simulation variables
-	real(dp) :: U0, L0, Ui, Li	!Conserved quantities variables
-	logical :: estimated_time
-	character(len=24) :: current_time
-	real(sp) :: start_time, end_time, total_time	!Program timer variables
-	logical :: approaching_center		!Approaching Center
-	real(dp) :: closest_distance_scattering_center, distance_scattering_center
-	real(dp) :: closest_position_scattering_center(3)
-	integer(i8) :: num_simulations, k
+	
+	! Input/output files, filenames, and format string
 	character(len=*), parameter :: input_file = "input.txt", aux_file = "aux.txt"
-	character(len=10) :: K0_char, x0_char, b_char, dt_char!AT MOST 10 CHARACTERS
-	character(len=:), allocatable :: K0_char_trim, x0_char_trim, b_char_trim
-	character(len=:), allocatable :: dt_char_trim, output_file, info_output_file
+	character(len=10) :: K0_char, x0_char, b_char, dt_char ! 10 characters max
+	character(len=:), allocatable :: K0_char_trim, x0_char_trim
+	character(len=:), allocatable :: b_char_trim, dt_char_trim
+	character(len=:), allocatable :: output_file, info_output_file
+	character(len=80) :: format_string
+	
+	! I/O units
 	integer(i8), parameter :: input_values_unit = 11, input_chars_unit = 12
 	integer(i8), parameter :: output_unit = 13, info_output_unit = 14
-	character(len=80) :: format_string
+		
+	! Fixed input parameters
+	integer(i8) :: num_simulations, num_plot_ploints
+	real(dp) :: K0, K0f, x0, x0f	! Initial kinetic energy and horizontal distance
+	
+	! Variable input parameters
+	real(dp) :: b, dt							! Impact parameter and time step size
+	
+	! Theoretical trajectory computation
+	real(dp) :: a, c, e, xf, yf
+	
+	! Simulated trajectory computation variables
+	integer(i8) :: max_iterations
+	real(dp) :: rt(3)						! Target electron position
+	real(dp) :: r0(3), v0(3)		! Initial position and velocity
+	real(dp) :: ti, ri(3)				! Time and position
+	real(dp) :: vi(3)						! Velocity
+	real(dp) :: ai(3) 					! Acceleration
+	
+	! Conserved quantities computation
+	real(dp) :: U0, L0, Ui, Li
+		
+	! Scattering angles computation and comparison
+	real(dp) :: scattering_angle_theoretical
+	real(dp) :: scattering_angle_plot, scattering_angle_simulation
+	
+	! Simulation timing
+	logical :: estimated_time
+	character(len=24) :: current_time
+	real(sp) :: start_time, end_time, total_time
+	
+	! Approach to scattering center detection and measurement
+	logical :: approaching_center
+	real(dp) :: closest_distance_scattering_center, distance_scattering_center
+	real(dp) :: closest_position_scattering_center(3)
+	
+	! Program structure variables
+	integer(i8) :: i, j, k
 
 	!Reading fixed parameters from input file
 	!Open input file twice: unit 11 to use as numbers, unit 12 to use as chars
@@ -83,7 +105,7 @@ program rutherfhord_scattering_test_simulations
 	do k = 1, num_simulations
 		!Fixed parameter values
 		K0 = K0f
-		hd = hdf
+		x0 = x0f
 		!Reading variable parameters from input file
 		!Impact parameter, b [Å]
 		read(input_values_unit, *) b
@@ -232,7 +254,8 @@ program rutherfhord_scattering_test_simulations
 		write(info_output_unit, format_string) 100*dabs(scattering_angle_simulation-scattering_angle_plot)/scattering_angle_plot
 		!Comparison between SSA and TSA
 		format_string = "('Percent error between SSA and TSA: ', d12.4, '%')"
-		write(info_output_unit, format_string) 100*dabs(scattering_angle_simulation-scattering_angle_theoretical)/scattering_angle_theoretical
+		write(info_output_unit, format_string) &
+		100*dabs(scattering_angle_simulation-scattering_angle_theoretical)/scattering_angle_theoretical
 		!Comparison between PSA and TSA
 		format_string = "('Percent error between PSA and TSA: ', d12.4, '%')"
 		write(info_output_unit, format_string) 100*dabs(scattering_angle_plot-scattering_angle_theoretical)/scattering_angle_theoretical
@@ -441,7 +464,7 @@ program rutherfhord_scattering_test_simulations
 
 		phif = phif - PI											! Reflect to hyperbola frame
 		phi0 = -phif - 2*alpha								! Initial angle to start plotting
-		dphi = (phif - phi0) / N							! Angular step between points
+		dphi = (phif - phi0)/num_plot_ploints	! Angular step between points
 
 		do i = 0, num_plot_ploints
 			phii = phi0 + i*dphi
